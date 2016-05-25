94e46c7fec7d7ea188e7eaa3a1a35ce3230ebbba
Claudine <claudine.bananal@gmail.com>
2016-05-25 14:00:14 -0700

    add about page with custom fields

---
+++ themes/inhabitent-v1/about.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *	Template Name: About
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+				<h1>Our Story</h1>
+        <p><?php echo CFS()->get( 'our_story' ); ?></p>
+				<h1>Our Team</h1>
+				<p><?php echo CFS()->get( 'our_team' ); ?></p>
+
+			<?php endwhile; // End of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
