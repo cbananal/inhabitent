2e094a65c5bdc2ffe90c60d9fa33b3ea080ffe65
Claudine <claudine.bananal@gmail.com>
2016-05-26 22:52:55 -0700

    refactor code for featured-entries section, remove unnecessary comments

--- themes/inhabitent-v1/front-page.php
+++ themes/inhabitent-v1/front-page.php
@@ -31,45 +31,7 @@ get_header(); ?>
 			</section>
 			<!--end section shop-container-->
 			<?php endif; ?>
-
-					<!--JS Version -->
-					<?php
-						// $terms = get_terms('product-type');
-						// if ( !empty($terms) && !is_wp_error($terms) ) {
-						// 		foreach ($terms as $term) {
-						// 			echo '<div><img src="' . get_template_directory_uri() . '/images/product-type-icons/' . $term->slug . '.svg"/><p>' . $term->description . '</p>' .
-						// 			'<a href="#">' .	$term->name . ' stuff' . '</a></div>';
-						// 		} //end foreach
-						// }
-					?>
-
-					<!--STATIC HTML BELOW-->
-					<!-- <div class="do">
-						<img src="<?php bloginfo('template_url') ?>/images/product-type-icons/do.svg">
-						<p>Get back to nature with all the tools and toys you need to enjoy the great outdoors.</p>
-						<a href="#">Do Stuff</a>
-					</div> -->
-					<!--end do-->
-					<!-- <div class="eat">
-						<img src="<?php bloginfo('template_url') ?>/images/product-type-icons/eat.svg">
-						<p>Nothing beats food cooked over a fire. We have all you need for good camping eats.</p>
-						<a href="#">Eat Stuff</a>
-					</div> -->
-					<!--end eat-->
-					<!-- <div class="sleep">
-						<img src="<?php bloginfo('template_url') ?>/images/product-type-icons/sleep.svg">
-						<p>Get a good night's rest in the wild in a home away from home that travels well.</p>
-						<a href="#">Sleep Stuff</a>
-					</div> -->
-					<!--end sleep-->
-					<!-- <div class="wear">
-						<img src="<?php bloginfo('template_url') ?>/images/product-type-icons/wear.svg">
-						<p>From flannel shirts to toques, look the part while roughing it in the great outdoors.</p>
-						<a href="#">Wear Stuff</a>
-					</div> -->
-					<!--end wear-->
-
-			<section class="featured-entries">
+			<section>
 				<h1>Inhabitent Journal</h1>
 				<div class="entries-container">
 					<?php
@@ -79,10 +41,7 @@ get_header(); ?>
 					<?php foreach ( $journal_posts as $post ) : setup_postdata( $post ); ?>
 					    <!-- //Content from your array of posts go here. -->
 							<div class="entry-box">
-								<div class="entry-thumbnail">
 									<?php the_post_thumbnail(); ?>
-								</div>
-								<!--end entry-thumbnail-->
 								<div class="entry-info"
 									<p><?php the_date('j F Y') ?> / <?php echo comments_number(); ?></p>
 									<h4><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></h4>
@@ -92,25 +51,6 @@ get_header(); ?>
 							</div>
 							<!--end entry-box-->
 					<?php endforeach; wp_reset_postdata(); ?>
-
-					<!-- <div class="entry-one">
-						<img src="">
-						<p>16 April 2016 / 3 comments</p>
-						<h4> <a class="title" href="#">Van Camping Photo Contest</a></h4>
-						<a href="#">Read Entry</a>
-					</div>
-					<div class="entry-two">
-						<img src="">
-						<p>2 April 2016 / 0 Comments</p>
-						<h4><a class="title" href="#">Fireside Libations: 3 Warm Cocktail Recipes</a></h4>
-						<a href="#">Read Entry</a>
-					</div>
-					<div class="entry-three">
-						<img src="">
-						<p>31 March 2016 / 0 Comments</p>
-						<h4><a class="title" href="#">How To: Eating Healthy Meals in the Wild</a></h4>
-						<a href="#">Read Entry</a>
-					</div> -->
 				</div>
 				<!--end entries-container-->
 			</section>
