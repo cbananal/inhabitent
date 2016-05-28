0d05cea89c1c1b013198a01a281bdbc19acf9908
Claudine <claudine.bananal@gmail.com>
2016-05-27 17:03:04 -0700

    fix structure and add styling on taxonomy page

--- themes/inhabitent-v1/taxonomy.php
+++ themes/inhabitent-v1/taxonomy.php
@@ -9,35 +9,28 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
-      <h1><?php single_term_title(); ?></h1>
-      <p><?php echo term_description(); ?></p>
+			<div class="product-heading">
+	      <h1><?php single_term_title(); ?></h1>
+	      <p><?php echo term_description(); ?></p>
+			</div>
+			<div class="product-list">
 			<?php while ( have_posts() ) : the_post(); ?>
-
-        <?php if ( has_post_thumbnail() ) : ?>
-    			<?php the_post_thumbnail( 'large' ); ?>
-    		<?php endif; ?>
-        
-        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-        	<header class="entry-header">
-        		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-        	</header><!-- .entry-header -->
-
-        	<div class="entry-content">
-        		<?php the_content(); ?>
-        		<?php
-        			wp_link_pages( array(
-        				'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
-        				'after'  => '</div>',
-        			) );
-        		?>
-        	</div><!-- .entry-content -->
-        </article><!-- #post-## -->
-
+				<div class="product-info-box">
+	        <?php if ( has_post_thumbnail() ) : ?>
+	    				<div class="product-photo">
+								<a href="<?php echo get_permalink(); ?>"><?php the_post_thumbnail('large') ?></a>
+							</div>
+	    		<?php endif; ?>
+	        	<div class="product-info">
+							<span class="product-title"><?php the_title(); ?></span>
+							<span class="product-price"><?php echo CFS()->get( 'price' ); ?></span>
+						</div>
+				</div>
+				<!--end product-info-box-->
 			<?php endwhile; // End of the loop. ?>
-
+			</div>
+			<!--end product-list-->
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
