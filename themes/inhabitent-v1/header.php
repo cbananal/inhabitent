3a9336d7fa0632109feacadcc6f028b032c9a3dc
Claudine <claudine.bananal@gmail.com>
2016-07-05 13:25:23 -0700

    fix broken php tag for search form on header.php

--- themes/inhabitent-v1/header.php
+++ themes/inhabitent-v1/header.php
@@ -32,7 +32,7 @@
 					<nav id="site-navigation" class="main-navigation" role="navigation">
 						<!-- <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button> -->
 						<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-						<? get_search_form(); ?>
+						<?php get_search_form(); ?>
 					</nav>
 					<!-- #site-navigation -->
 				</div>
