8f0eee3ecbac42a66875637ad7b017689983b173
Claudine <claudine.bananal@gmail.com>
2016-07-05 19:20:12 -0700

    revert to the original template of archive.php and enlarge thumbnails

--- themes/inhabitent-v1/archive.php
+++ themes/inhabitent-v1/archive.php
@@ -1,9 +1,9 @@
 <?php
 /**
-* The main template file.
-*
-* @package RED_Starter_Theme
-*/
+ * The template for displaying archive pages.
+ *
+ * @package RED_Starter_Theme
+ */
 
 get_header(); ?>
 
@@ -12,40 +12,20 @@ get_header(); ?>
 
 		<?php if ( have_posts() ) : ?>
 
-			<?php if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-			<?php endif; ?>
+			<header class="page-header">
+				<?php
+					the_archive_title( '<h1 class="page-title">', '</h1>' );
+					the_archive_description( '<div class="taxonomy-description">', '</div>' );
+				?>
+				
+			</header><!-- .page-header -->
 
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<header class="entry-header">
-						<!-- <?php
-							the_archive_title( '<h1 class="page-title">', '</h1>' );
-							the_archive_description( '<div class="taxonomy-description">', '</div>' );
-						?> -->
-						<?php if ( has_post_thumbnail() ) : ?>
-							<?php the_post_thumbnail( 'full' ); ?>
-						<?php endif; ?>
-
-						<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-
-						<?php if ( 'post' === get_post_type() ) : ?>
-						<div class="entry-meta">
-							<?php the_time('j F Y'); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php red_starter_posted_by(); ?>
-						</div><!-- .entry-meta -->
-						<?php endif; ?>
-					</header><!-- .entry-header -->
-
-					<div class="entry-content">
-						<?php the_excerpt(); ?>
-						<a class="secondary-button" href="<?php echo get_permalink(); ?>">Read More &rarr;</a>
-					</div><!-- .entry-content -->
-				</article><!-- #post-## -->
+				<?php
+					get_template_part( 'template-parts/content' );
+				?>
 
 			<?php endwhile; ?>
 
